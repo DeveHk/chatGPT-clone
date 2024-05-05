1ccaa5e60494735e3a5e4854302bff805b4246b5
deveHk <connect.to.hk@gmail.com>
2024-05-05 23:10:17 +0530

    Home page redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    return redirect("/chats");
+  } else {
+    return redirect("/sign-in");
+  }
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
-      <div className="">Login</div>
+      <div className="">HOME</div>
     </div>
   );
 }
